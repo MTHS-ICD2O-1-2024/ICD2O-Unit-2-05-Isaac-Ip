c9ad6329d607b568902630ac64f3e0d7756104ef
default <isaac.ip@stu.ocsb.ca>
2025-03-21 17:20:52 +0000

    unit 2-05

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates pay and tax.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateTax () {
+  // input
+  const hoursWorked = parseFloat(document.getElementById('hours-worked').value)
+  const hourlyWage = parseFloat(document.getElementById('hourly-wage').value)
+
+  // process
+  const amountOfPay = (hoursWorked * hourlyWage) * (1 - 0.18)
+  const amountOfTaxes = (hoursWorked * hourlyWage) * 0.18
+
+  // output
+  document.getElementById('pay').innerHTML = 'Your pay will be: $' + amountOfPay.toFixed(2)
+  document.getElementById('taxes').innerHTML = 'The government will take: $' + amountOfTaxes.toFixed(2)
 }
